d09db4f5fed740a4498e78e3e348e11e06aa9bc6
Miche <gmespace.74@gmail.com>
2023-01-16 18:36:09 +0000

    pmkolk

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ app.use(session({
     store: store,
     proxy: true, // Required for Heroku & Digital Ocean (regarding X-Forwarded-For)
     name: 'MyCoolWebAppCookieName', // This needs to be unique per-host.
-    cookie: { httpOnly: false, secure: true, maxAge: 1000 * 60 * 60 * 48, sameSite: 'none' } 
+    cookie: { httpOnly: true, secure: true, maxAge: 1000 * 60 * 60 * 48, sameSite: 'none' } 
 }));
 app.use(cors({
     credentials: true,
