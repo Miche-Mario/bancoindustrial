c372b3f5d0924fc680fb36ab0d5b56cacb1a2f6e
Miche <gmespace.74@gmail.com>
2023-01-17 10:32:22 +0000

    iuhghhu

--- index.js
+++ index.js
@@ -62,9 +62,7 @@ app.use(session({
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'https://www.banco-industrial.net');
     res.header('Access-Control-Allow-Credentials', 'true');
-
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-   
     next();
   });
 
